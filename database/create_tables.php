5242e38323778ab51e9980808275e40cb88eeda8
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2023-09-28 15:33:45 -0300

    Update create_tables.php

---
+++ database/create_tables.php
@@ -0,0 +1,18 @@
+<?php
+    include_once 'db_connect.php';//inclui o arquivo de conexão com o banco de dados
+    $sql = "CREATE TABLE jogos (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(255) NOT NULL,
+        edicao VARCHAR(255),
+        status ENUM('adquirido', 'backlog') NOT NULL,
+        lojas VARCHAR(255),
+        plataformas VARCHAR(255),
+        descricao TEXT,
+        logo_url VARCHAR(255)
+    )";//comando SQL para criar a tabela de jogos
+    if(mysqli_query($conn, $sql)){//caso a tabela seja criada com sucesso, exibe uma mensagem de sucesso
+        echo "Tabela de jogos criada com sucesso!";//mensagem de sucesso
+    }else{//caso ocorra algum erro, exibe uma mensagem de erro
+        echo "Erro ao criar a tabela de jogos: " . mysqli_error($conn);//mensagem de erro
+    }
+?>
