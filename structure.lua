a1594baf24b34895a39c486effa7b220c03649c1
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2023-09-28 15:25:43 -0300

    Create structure.lua

---
+++ structure.lua
@@ -0,0 +1,23 @@
+project/
+|-- index.php
+|-- cadastro_jogo.php
+|-- adquiridos.php
+|-- backlog.php
+|-- download.php
+|-- includes/
+|   |-- header.php
+|   |-- footer.php
+|-- api/
+|   |-- rawg_api.php
+|-- database/
+|   |-- db_config.php
+|   |-- db_connect.php
+|   |-- create_tables.php
+|-- assets/
+|   |-- images/
+|       |-- plataforma1.jpg
+|       |-- plataforma2.jpg
+|       |-- loja1.jpg
+|       |-- loja2.jpg
+|-- css/
+|   |-- style.css
